869b641a5ce760dea3bb2394bc4a3bcb838ba98e
1009347 <TI1009347@wcupa.edu>
2024-09-13 22:44:39 -0400

    Update Q3PrintArrayBackwards.java

--- src/main/java/Q3PrintArrayBackwards.java
+++ src/main/java/Q3PrintArrayBackwards.java
@@ -2,7 +2,7 @@ public class Q3PrintArrayBackwards {
     private static int[] array = {1, 2, 3, 4, 5, 6, 7};
 
     public static void main(String[] args) {
-        printRecursion(array.length);
+        printRecursion(array.length - 1);
     }
 
     private static void printRecursion(int i) {
@@ -10,7 +10,7 @@ public class Q3PrintArrayBackwards {
         // Hint: The base case is when i <= 0
         // Hint: Print the element after the recursive call
         if (i < 0) {
-            return;
+            return; 
         }
         System.out.println(array[i]); 
         printRecursion(i - 1);
