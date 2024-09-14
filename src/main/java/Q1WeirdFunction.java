fd0b7c63a3468057dba7bf91d4a8b192d409228f
1009347 <TI1009347@wcupa.edu>
2024-09-13 23:05:49 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -2,41 +2,43 @@
 
 public class Q1WeirdFunction {
 
-    public static int fRecursive(int n) {
+    public static int recursiveF(int n) {
         // TODO: Implement the recursive function
         if (n < 3) {
             return n;
         }
-        return fRecursive(n - 1) + 2 * fRecursive(n - 2) + 3 * fRecursive(n - 3);
+        return recursiveF(n - 1) + 2 * recursiveF(n - 2) + 3 * recursiveF(n - 3);
         
         //return 0;
     }
 
-    public static int fIterative(int n) {
+    public static int iterativeF(int n) {
         // TODO: Implement the iterative function
         if (n < 3) {
             return n;
         }
 
-        int f0 = 0; // f(0)
-        int f1 = 1; // f(1)
-        int f2 = 2; // f(2)
-        int result = 0;
+        
+        int[] f = new int[n + 1];
+        f[0] = 0;
+        f[1] = 1;
+        f[2] = 2;
 
+        
         for (int i = 3; i <= n; i++) {
-            result = f2 + 2 * f1 + 3 * f0;
-            f0 = f1;
-            f1 = f2;
-            f2 = result;
+            f[i] = f[i - 1] + 2 * f[i - 2] + 3 * f[i - 3];
+        }
+
+        return f[n];
         //return 0;
     }
-    return result;
-   }
+
    
    public static void main(String[] args) {
-        System.out.println("Recursive result for n=4: " + fRecursive(4));
+        int n = 5;
+        System.out.println("Recursive f(" + n + ") = " + recursiveF(n));
 
-        System.out.println("Iterative result for n=4: " + fIterative(4));
+        System.out.println("Iterative f(" + n + ") = " + iterativeF(n));
     }
     
 }
